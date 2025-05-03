8e3a47a1aca8f7f8758fa2bd89a5c3a2905640a6
congcongyang <hdndzy@ncepu.edu.cn>
2025-05-03 21:52:17 +0800

    Update CalcuOpCR_FixOutTemp.m

--- Land_Efficiency_Based/GetBestCR_LandEfficiency/CalcuOpCR_FixOutTemp.m
+++ Land_Efficiency_Based/GetBestCR_LandEfficiency/CalcuOpCR_FixOutTemp.m
@@ -3,8 +3,8 @@ function result = CalcuOpCR_FixOutTemp(SolarIR, envTemp, FixOutTemp)
     X_Dim = 1;
 
     %% 优化
-    zhongqun = 50;
-    cishu = 5000;
+    zhongqun = 200;
+    cishu = 20000;
     
     f1 = @(CR)0.0 - calculate_WholeOutput_FixOutTemp_EnvIntemp(CR, SolarIR, envTemp, FixOutTemp) / (1 + CR * CR);   % 环境入口温度
     % f1 = @(CR)0.0 - calculate_WholeOutput_FixOutTemp_SetIntemp(CR, SolarIR, FixOutTemp) / (1 + CR * CR);   % 设定入口温度
